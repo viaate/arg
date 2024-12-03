6c176bd5e450937ce2fce0517b3a8159f4b046aa
Olivia Gonsher <81873934+viaate@users.noreply.github.com>
2024-12-03 14:45:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function revealLink() {
+    document.getElementById('message').innerHTML = `
+        <h1>Welcome to the Echo</h1>
+        <p>The first step is to find the door. Enter the code to continue...</p>
+        <input type="text" id="codeInput" placeholder="Enter code..." />
+        <button onclick="checkCode()">Submit</button>
+    `;
+}
+
+function checkCode() {
+    const code = document.getElementById('codeInput').value;
+    if (code === '7421') {
+        window.location.href = 'clue2.html';
+    } else {
+        alert('Incorrect code. Try again.');
+    }
+}
 
