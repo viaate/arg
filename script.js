4dc7717c53c8af1b386a39da9c5ce06ddd4e6fe3
Olivia Gonsher <81873934+viaate@users.noreply.github.com>
2024-12-03 14:50:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+// This function will reveal the next part of the game when the user clicks "Enter"
 function revealLink() {
+    // Change the content of the message to reveal the next step
     document.getElementById('message').innerHTML = `
         <h1>Welcome to the Echo</h1>
         <p>The first step is to find the door. Enter the code to continue...</p>
@@ -7,12 +9,5 @@ function revealLink() {
     `;
 }
 
-function checkCode() {
-    const code = document.getElementById('codeInput').value;
-    if (code === '7421') {
-        window.location.href = 'clue2.html';
-    } else {
-        alert('Incorrect code. Try again.');
-    }
-}
-
+// Ensure that the "Enter" button actually triggers the function when clicked
+document.getElementById('enterButton').addEventListener('click', revealLink);
